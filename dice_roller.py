30d539684bfad0bc92790131da4bad2b19f50525
Stockriii <mori.haase1@gmail.com>
2021-02-03 14:41:48 +0100

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,11 @@
-def main():
-  print('You rolled a die')
+import random
 
-if __name__== "__main__":
-  main()
+dice_rolls = 2
+dice_sum = 0
+
+for i in range(0,dice_rolls):
+  roll = random.randint(1,6)
+  print(f'You rolled a {roll}')
+  dice_sum += roll
+
+print(f"This is the sum of both rolls {dice_sum}")
