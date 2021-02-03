0d6f5d6b9161f2e98cdf6e5d1fcfa558d40fadf3
Stockriii <mori.haase1@gmail.com>
2021-02-03 14:56:39 +0100

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -5,7 +5,12 @@ dice_sum = 0
 
 for i in range(0,dice_rolls):
   roll = random.randint(1,6)
-  print(f'You rolled a {roll}')
+  if roll ==1:
+    print(f'You rolled a {roll}! Critical Failure')
+  elif roll == 6:
+    print(f'You rolled a {roll}! Critical Win')
+  else:
+    print(f"You rolled a {roll}")
   dice_sum += roll
 
 print(f"This is the sum of both rolls {dice_sum}")
